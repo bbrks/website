db54d6e33e7318c34c0a1a630a3fadf71ccddbda
Ben Brooks <ben@bbrks.me>
2012-12-10 23:53:02 +0000

    Added GitHub banner

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 	</script>
 </head>
 <body>
+	<a href="https://github.com/bbrks/website"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png" alt="Fork me on GitHub"></a>
 	<div id="content">
 		<h1><a href="">Ben Brooks</a></h1>
 		<ul>
