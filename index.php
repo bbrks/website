d42bb80837183f43a85ca6bcd01ca3ddcd328d7e
Ben Brooks <ben@bbrks.me>
2012-12-10 22:37:38 +0000

    Added HTML/CSS

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Ben Brooks</title>
+	<link rel="stylesheet" href="res/style.css" />
+</head>
+<body>
+	<div id="content">
+		<h1><a href="">Ben Brooks</a></h1>
+		<ul>
+			<li><a href="http://facebook.com/bbrks" title="Ben Brooks on Facebook">f</a></li>
+			<li><a href="http://uk.linkedin.com/in/bbrks" title="Ben Brooks on LinkedIn">l</a></li>
+			<li><a href="http://github.com/bbrks" title="Ben Brooks on GitHub">g</a></li>
+			<li><a href="http://twitter.com/bbrks_" title="Ben Brooks on Twitter">T</a></li>
+			<li><a href="http://flickr.com/bbrks" title="Ben Brooks on Flickr">F</a></li>
+			<li><a href="http://last.fm/user/bbrks_" title="Ben Brooks on LastFM">L</a></li>
+			<li><a href="mailto:ben@bbrks.me" title="E-Mail Me!">&#x2709;</a></li>
+		</ul>
+	</div>
+</body>
+</html>
