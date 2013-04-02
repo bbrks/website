1d10465bc7b3adca1826f1b38103d9fe2d3ab44b
Ben Brooks <ben@bbrks.me>
2013-04-02 01:17:21 +0100

    Shiny new site

---
+++ index.php
@@ -0,0 +1,121 @@
+<!--
+
+	This code was thrown together in a few hours, please don't judge me on this! ;)
+
+ -->
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Ben Brooks &#124; Portfolio</title>
+		<link rel="stylesheet" href="res/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="res/css/style.css" />
+	</head>
+	<body>
+
+		<div class="whitebg">
+			<div class="container">
+				<div class="row">
+					<div class="span7">
+						<h1>
+							<img src="res/img/me.jpg" class="avatar img-circle" />
+							<a href="http://bbrks.me">Ben Brooks</a>
+							<span style="color:#eee;"> &#124; </span>
+							<span style="color:#666;font-size:0.7em;">Portfolio</span>
+						</h1>
+					</div>
+					<div class="offset2 span3 text-right">
+						<ul class="social inline">
+							<li><a href="http://facebook.com/bbrks" title="Ben Brooks on Facebook">f</a></li>
+							<li><a href="http://twitter.com/bbrks_" title="Ben Brooks on Twitter">T</a></li>
+							<li><a href="http://github.com/bbrks" title="Ben Brooks on GitHub">g</a></li>
+							<li><a href="http://uk.linkedin.com/in/bbrks" title="Ben Brooks on LinkedIn">l</a></li>
+							<li><a href="mailto:ben@bbrks.me" title="E-Mail Me!">&#x2709;</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="hspace text-center">
+			<h1>Beautiful, Elegant &amp; Clean Designs.</h1>
+		</div>
+
+		<div class="whitebg">
+			<div class="container">
+				<div class="row">
+					<div class="span12">
+						<h2><a href="http://tf2dingalings.com">TF2Dingalings.com</a></h2>
+						<p class="lead">A community driven website based around sharing audio snippets to use in their games.</p>
+						<p>Integrated with <a href="http://steamcommunity.com">Steam Community</a> and running on a custom built CMS. As seen on <a href="http://www.pcgamer.com/2012/08/14/the-origin-of-call-of-dutys-most-heard-sound/">PCGamer.com</a> and <a href="http://www.tfportal.de/news/1555/tfportal-fanstuff-spezial-teil-1-websites/">tfportal.de</a>.</p>
+					</div>
+				</div>
+				<div class="macbook">
+					<img src="res/img/tf2d.jpg" />
+					<img src="res/img/glare.png" />
+				</div>
+			</div>
+		</div>
+
+		<div class="hspace text-center">
+			<h1>Accessible, Semantic &amp; Flexible Code.</h1>
+		</div>
+
+		<div class="whitebg">
+			<div class="container">
+				<div class="row">
+					<div class="span12">
+						<h2><a href="http://users.aber.ac.uk/beb12/cs25010/">abergaemz</a></h2>
+						<p class="lead">A clean, minimalistic design for an online video game shop.</p>
+						<p>Created for a university assignment, it features a PHP backend interacting with a SQL database, as well as JS for front-end effects and form validation.</p>
+					</div>
+				</div>
+				<div class="macbook">
+					<img src="res/img/abergaemz.jpg" />
+					<img src="res/img/glare.png" />
+				</div>
+			</div>
+		</div>
+
+		<div class="hspace text-center">
+			<h1>Effective branding.</h1>
+		</div>
+
+		<div class="whitebg">
+			<div class="container">
+				<div class="row">
+					<div class="span12">
+						<h2>AberPhotoSoc Branding</a></h2>
+						<p class="lead">Clean and smooth iconography help draw the eye and let the viewer fill in the blanks.</p>
+						<p>Branding for the Aberystwyth Photography Society to be used on posters and fliers around campus and town.</p>
+					</div>
+				</div>
+				<div class="macbook">
+					<img src="res/img/aberphotosoc.jpg" />
+					<img src="res/img/glare.png" />
+				</div>
+			</div>
+		</div>
+
+		<div class="hspace text-center">
+			<h1>TODO</h1>
+		</div>
+
+		<div class="whitebg">
+			<div class="container">
+				<div class="row">
+					<div class="offset4 span4 text-center" style="color:#666;">
+						<ul class="social inline">
+							<li><a href="http://facebook.com/bbrks" title="Ben Brooks on Facebook">f</a></li>
+							<li><a href="http://twitter.com/bbrks_" title="Ben Brooks on Twitter">T</a></li>
+							<li><a href="http://github.com/bbrks" title="Ben Brooks on GitHub">g</a></li>
+							<li><a href="http://uk.linkedin.com/in/bbrks" title="Ben Brooks on LinkedIn">l</a></li>
+							<li><a href="mailto:ben@bbrks.me" title="E-Mail Me!">&#x2709;</a></li>
+						</ul>
+						<p>&copy; Ben Brooks 2013</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
