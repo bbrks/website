bc6e78817fab9fb83bc103efd725c243c878769f
Ben Brooks <ben@bbrks.me>
2013-04-04 01:22:44 +0100

    Rewritten for IE compatability

--- index.php
+++ index.php
@@ -1,208 +1,87 @@
-<!--
-
-	This code was thrown together in a few hours, please don't judge me on this! ;)
-
- -->
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="utf-8" />
 		<title>Ben Brooks &#124; Portfolio</title>
-		<link rel="stylesheet" href="res/css/bootstrap.min.css" />
-		<link rel="stylesheet" href="res/css/style.css" />
+		<link rel="stylesheet" href="res/css/style.min.css" />
 	</head>
 	<body>
 
-		<div class="whitebg">
+		<div class="row">
 			<div class="container">
-				<div class="row">
-					<div class="span7">
-						<h1>
-							<img src="res/img/me.jpg" class="avatar img-circle" />
-							<a href="http://bbrks.me">Ben Brooks</a>
-							<span style="color:#eee;"> &#124; </span>
-							<span style="color:#666;font-size:0.7em;">Portfolio</span>
-						</h1>
-					</div>
-					<div class="offset2 span3 text-right">
-						<ul class="social inline">
-							<li><a href="http://facebook.com/bbrks" title="Ben Brooks on Facebook">f</a></li>
-							<li><a href="http://twitter.com/bbrks_" title="Ben Brooks on Twitter">T</a></li>
-							<li><a href="http://github.com/bbrks" title="Ben Brooks on GitHub">g</a></li>
-							<li><a href="http://uk.linkedin.com/in/bbrks" title="Ben Brooks on LinkedIn">l</a></li>
-							<li><a href="mailto:ben@bbrks.me" title="E-Mail Me!">&#x2709;</a></li>
-						</ul>
-					</div>
+				<div class="grid_3">
+					<img src="res/img/me.jpg" class="avatar" />
+					<h1><a href=".">Ben Brooks</a></h1>
+					<h2>Portfolio</h2>
 				</div>
+				<div class="grid_2">
+					<ul class="social">
+						<li><a href="http://facebook.com/bbrks" title="Ben Brooks on Facebook">f</a></li>
+						<li><a href="http://twitter.com/bbrks_" title="Ben Brooks on Twitter">T</a></li>
+						<li><a href="http://github.com/bbrks" title="Ben Brooks on GitHub">g</a></li>
+						<li><a href="http://uk.linkedin.com/in/bbrks" title="Ben Brooks on LinkedIn">l</a></li>
+						<li><a href="mailto:ben@bbrks.me" title="E-Mail Me!">&#x2709;</a></li>
+					</ul>
+				</div>
+				<div class="clear"></div>
 			</div>
 		</div>
 
-		<div class="hspace text-center">
-			<h1>Beautiful, Elegant &amp; Clean Designs.</h1>
+		<div class="row2">
+			<h4>Beautiful, Elegant &amp; Clean Design.</h4>
 		</div>
 
-		<div class="whitebg">
+		<div class="row">
 			<div class="container">
-				<div class="row">
-					<div class="span12">
-						<h2><a href="http://tf2dingalings.com">TF2Dingalings.com</a></h2>
-						<p class="lead">A community driven website based around sharing audio snippets to use in their games.</p>
-						<p>Integrated with <a href="http://steamcommunity.com">Steam Community</a> and running on a custom built CMS. As seen on <a href="http://www.pcgamer.com/2012/08/14/the-origin-of-call-of-dutys-most-heard-sound/">PCGamer.com</a> and <a href="http://www.tfportal.de/news/1555/tfportal-fanstuff-spezial-teil-1-websites/">tfportal.de</a>.</p>
-					</div>
-				</div>
+				<h3><a href="http://tf2dingalings.com">TF2Dingalings.com</a></h3>
+				<p class="lead">A community driven website based around sharing sound bites to use in games.</p>
+				<p>Integrated with <a href="http://steamcommunity.com">Steam Community</a> and running on a custom built PHP CMS. As seen on <a href="http://www.pcgamer.com/2012/08/14/the-origin-of-call-of-dutys-most-heard-sound/">PCGamer.com</a> and <a href="http://www.tfportal.de/news/1555/tfportal-fanstuff-spezial-teil-1-websites/">tfportal.de</a>.</p>
+			</div>
+			<div class="promotf2d promocols3 promo"></div>
+		</div>
 
-				<div id="tf2dCarousel" class="carousel slide">
-					<ol class="carousel-indicators">
-						<li data-target="#tf2dCarousel" data-slide-to="0" class="active"></li>
-						<li data-target="#tf2dCarousel" data-slide-to="1"></li>
-						<li data-target="#tf2dCarousel" data-slide-to="2"></li>
-					</ol>
-					<div class="carousel-inner">
-						<div class="active item">
-							<div class="macbook">
-								<img src="res/img/tf2d.jpg" />
-								<img src="res/img/glare.png" />
-							</div>
-						</div>
-						<div class="item">
-							<div class="macbook">
-								<img src="res/img/tf2d2.jpg" />
-								<img src="res/img/glare.png" />
-							</div>
-						</div>
-						<div class="item">
-							<div class="macbook">
-								<img src="res/img/tf2d3.jpg" />
-								<img src="res/img/glare.png" />
-							</div>
-						</div>
-					</div>
-					<a class="carousel-control left" href="#tf2dCarousel" data-slide="prev">&lsaquo;</a>
- 					<a class="carousel-control right" href="#tf2dCarousel" data-slide="next">&rsaquo;</a>
-				</div>
+		<div class="row2">
+			<h4>Accessible, Semantic &amp; Flexible Code.</h4>
+		</div>
 
+		<div class="row">
+			<div class="container">
+				<h3><a href="http://users.aber.ac.uk/beb12/cs25010/">abergaemz</a></h3>
+				<p class="lead">A clean, minimalistic design for an online video game shop.</p>
+				<p>Created for a university assignment, it features a PHP backend interacting with a SQL database, as well as JS for front-end effects and form validation.</p>
+			</div>
+			<div class="promoabergaemz promocols2 promo"></div>
+		</div>
+
+		<div class="row2">
+			<h4>Effective branding.</h4>
+		</div>
+
+		<div class="row">
+			<div class="container">
+				<h3>AberPhotoSoc Branding</a></h3>
+				<p class="lead">Clean and smooth iconography help draw the eye and let the viewer fill in the blanks.</p>
+				<p>Branding for the Aberystwyth Photography Society to be used on posters and fliers around campus and town.</p>
+			</div>
+			<div class="promoaberphoto promocols2 promo"></div>
+		</div>
+
+		<div class="row2">
+			<h4>Pixel Perfection.</h4>
+		</div>
+
+		<div class="row">
+			<div class="footer container">
+				<ul class="social">
+					<li><a href="http://facebook.com/bbrks" title="Ben Brooks on Facebook">f</a></li>
+					<li><a href="http://twitter.com/bbrks_" title="Ben Brooks on Twitter">T</a></li>
+					<li><a href="http://github.com/bbrks" title="Ben Brooks on GitHub">g</a></li>
+					<li><a href="http://uk.linkedin.com/in/bbrks" title="Ben Brooks on LinkedIn">l</a></li>
+					<li><a href="mailto:ben@bbrks.me" title="E-Mail Me!">&#x2709;</a></li>
+				</ul>
+				<p>&copy; Ben Brooks 2013</p>
 			</div>
 		</div>
 
-		<div class="hspace text-center">
-			<h1>Accessible, Semantic &amp; Flexible Code.</h1>
-		</div>
-
-		<div class="whitebg">
-			<div class="container">
-				<div class="row">
-					<div class="span12">
-						<h2><a href="http://users.aber.ac.uk/beb12/cs25010/">abergaemz</a></h2>
-						<p class="lead">A clean, minimalistic design for an online video game shop.</p>
-						<p>Created for a university assignment, it features a PHP backend interacting with a SQL database, as well as JS for front-end effects and form validation.</p>
-					</div>
-				</div>
-
-				<div id="abergaemzCarousel" class="carousel slide">
-					<ol class="carousel-indicators">
-						<li data-target="#abergaemzCarousel" data-slide-to="0" class="active"></li>
-						<li data-target="#abergaemzCarousel" data-slide-to="1"></li>
-					</ol>
-					<div class="carousel-inner">
-						<div class="active item">
-							<div class="macbook">
-								<img src="res/img/abergaemz.jpg" />
-								<img src="res/img/glare.png" />
-							</div>
-						</div>
-						<div class="item">
-							<div class="macbook">
-								<img src="res/img/abergaemz2.jpg" />
-								<img src="res/img/glare.png" />
-							</div>
-						</div>
-					</div>
-					<a class="carousel-control left" href="#abergaemzCarousel" data-slide="prev">&lsaquo;</a>
- 					<a class="carousel-control right" href="#abergaemzCarousel" data-slide="next">&rsaquo;</a>
-				</div>
-
-			</div>
-		</div>
-
-		<div class="hspace text-center">
-			<h1>Effective branding.</h1>
-		</div>
-
-		<div class="whitebg">
-			<div class="container">
-				<div class="row">
-					<div class="span12">
-						<h2>AberPhotoSoc Branding</a></h2>
-						<p class="lead">Clean and smooth iconography help draw the eye and let the viewer fill in the blanks.</p>
-						<p>Branding for the Aberystwyth Photography Society to be used on posters and fliers around campus and town.</p>
-					</div>
-				</div>
-
-				<div id="aberphotosocCarousel" class="carousel slide">
-					<ol class="carousel-indicators">
-						<li data-target="#aberphotosocCarousel" data-slide-to="0" class="active"></li>
-						<li data-target="#aberphotosocCarousel" data-slide-to="1"></li>
-					</ol>
-					<div class="carousel-inner">
-						<div class="active item">
-							<div class="macbook">
-								<img src="res/img/aberphotosoc.jpg" />
-								<img src="res/img/glare.png" />
-							</div>
-						</div>
-						<div class="item">
-							<div class="macbook">
-								<img src="res/img/aberphotosoc2.jpg" />
-								<img src="res/img/glare.png" />
-							</div>
-						</div>
-					</div>
-					<a class="carousel-control left" href="#aberphotosocCarousel" data-slide="prev">&lsaquo;</a>
- 					<a class="carousel-control right" href="#aberphotosocCarousel" data-slide="next">&rsaquo;</a>
-				</div>
-
-			</div>
-		</div>
-
-		<div class="hspace text-center">
-			<h1>Pixel Perfection.</h1>
-		</div>
-
-		<div class="whitebg">
-			<div class="container">
-				<div class="row">
-					<div class="offset4 span4 text-center" style="color:#666;">
-						<ul class="social inline">
-							<li><a href="http://facebook.com/bbrks" title="Ben Brooks on Facebook">f</a></li>
-							<li><a href="http://twitter.com/bbrks_" title="Ben Brooks on Twitter">T</a></li>
-							<li><a href="http://github.com/bbrks" title="Ben Brooks on GitHub">g</a></li>
-							<li><a href="http://uk.linkedin.com/in/bbrks" title="Ben Brooks on LinkedIn">l</a></li>
-							<li><a href="mailto:ben@bbrks.me" title="E-Mail Me!">&#x2709;</a></li>
-						</ul>
-						<p>&copy; Ben Brooks 2013</p>
-					</div>
-				</div>
-			</div>
-		</div>
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-		<script type="text/javascript" src="res/js/bootstrap.min.js"></script>
-		<script type="text/javascript">
-		$('.carousel').carousel({
-		  interval: 3000
-		})</script>
-		<script type="text/javascript">
-
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-3687044-6']);
-		  _gaq.push(['_setDomainName', 'bbrks.me']);
-		  _gaq.push(['_trackPageview']);
-
-		  (function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-
-		</script>
 	</body>
 </html>
